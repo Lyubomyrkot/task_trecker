302e4a2818b86e322df929903ec8966c40b4d3ea
Lyubomyrkot <lyubomyrkot@gmail.com>
2026-06-30 12:14:16 +0300

    зробив логін реєстрацію логаут та виправив баги на мобільній версії

--- task_tracker/urls.py
+++ task_tracker/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tasks.urls')),
+    path('', include('auth_system.urls')),
 ]
 
 if settings.DEBUG:
